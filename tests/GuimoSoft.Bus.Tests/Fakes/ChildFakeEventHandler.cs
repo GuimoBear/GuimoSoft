ec020581268e46b89f71ff9b45d7b8d95fe20bb8
Guimo <guigui2004@gmail.com>
2021-10-19 01:22:05 -0300

    Incremento dos testes unitários

--- tests/GuimoSoft.Bus.Tests/Fakes/ChildFakeEventHandler.cs
+++ tests/GuimoSoft.Bus.Tests/Fakes/ChildFakeEventHandler.cs
@@ -1,19 +1,25 @@
 ﻿using GuimoSoft.Bus.Abstractions;
 using GuimoSoft.Bus.Abstractions.Consumer;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace GuimoSoft.Bus.Tests.Fakes
 {
-    public class ChildFakeEventHandler : IEventHandler<ChildFakeEvent>
+    public class ChildFakeEventHandler : IEventHandler<ChildFakeEvent>, IEvent, IEquatable<string>
     {
         public Task Handle(ChildFakeEvent notification, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
+
+        public bool Equals(string other)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public class ChildFakeEventThrowExceptionHandler : IEventHandler<ChildFakeEvent>, IEvent
+    public class ChildFakeEventThrowExceptionHandler : IEventHandler<ChildFakeEvent>
     {
         public Task Handle(ChildFakeEvent notification, CancellationToken cancellationToken)
         {
